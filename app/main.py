2997144739c22853e65c13ea11bc0826c7e6ed6b
wjdrbs51@naver.com <wjdrbs51@naver.com>
2024-09-10 18:07:20 +0900

    init : fastapi를 위한 초기화 작업

--- app/main.py
+++ app/main.py
@@ -3,4 +3,17 @@
 FastAPI 실행을 위한 최상단 main 코드
 =====================================================================
 """
+from dotenv import load_dotenv
+import os
+
+import uvicorn
+from fastapi import FastAPI
+
+from core.directory_event_handler import Handler
+from core.document_processor import DocumentProcessor
+from core.vector_store import VectorStore
+from core.reranker import Reranker
+
+# .env 파일 로드
+load_dotenv()
 
